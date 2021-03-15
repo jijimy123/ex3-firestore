5c5bfa314f50e754c822dc067d640496fb7196a4
jijimy <jijimy123@gmail.com>
2021-03-15 17:13:08 -0400

    finir exe3

--- src/composants/ListeProduits.jsx
+++ src/composants/ListeProduits.jsx
@@ -1,30 +1,31 @@
-import './ListeProduits.scss';
+import "./ListeProduits.scss";
 import Produit from "./Produit";
-import { useEffect, useState } from 'react';
-/******* Ex#3 - Étape D ********************************/ 
+import { useEffect, useState } from "react";
+/******* Ex#3 - Étape D ********************************/
 // Importer l'objet bd du fichier firebase.js
-
+import bd from "../data/firebase";
 
 export default function ListeProduits(props) {
-  /******* Ex#3 - Étape E ********************************/ 
+  /******* Ex#3 - Étape E ********************************/
   // Créer un "état" React pour les produits (utiliser useState)
-  
-    
+  const [produits, setProduits] = useState([]);
   useEffect(() => {
     async function getProduits() {
       // On initialise un tableau pour contenir les produits extraits de Firestore
       const tabProduits = [];
-      
-      /******* Ex#3 - Étape F ********************************/ 
+
+      /******* Ex#3 - Étape F ********************************/
       // Faire une requête à la collection de produits sur Firestore et remplir les tableau tabProduits avec les données de produits retournées par Firestore
-      // [Suggestion : remarquez que la fonction getProduits() est marquée 'async'. Lorsque vous appelez la méthode Firestore qui retourne les produits, cette fonction 
-      // est une Promesse, vous pouvez simplement utiliser la syntax 'await' pour attendre le résultat avant de remplir le tableau tabProduits 
+      // [Suggestion : remarquez que la fonction getProduits() est marquée 'async'. Lorsque vous appelez la méthode Firestore qui retourne les produits, cette fonction
+      // est une Promesse, vous pouvez simplement utiliser la syntax 'await' pour attendre le résultat avant de remplir le tableau tabProduits
       // (visionnez la capsule au sujet du code asynchrone en JavaScript)]
 
-      
-      /******* Ex#3 - Étape G ********************************/ 
+      const promesse = await bd.collection("ex3-produits").get();
+      promesse.forEach((prd) => tabProduits.push(prd.data()));
+
+      /******* Ex#3 - Étape G ********************************/
       // Modifier l'état des produits (initialisé ci-dessus avec useState) en utilisant le mutateur et le tableau tabProduits
-      
+      setProduits(tabProduits);
     }
     getProduits();
   }, []); // Ne modifiez surtout pas le tableau des dépendances à gauche : vous risquez un appel récurent sans fin de l'API Firebase !!!!
@@ -32,7 +33,7 @@ export default function ListeProduits(props) {
   return (
     <div className="ListeProduits">
       <h2>Produits disponibles</h2>
-      <ul>        
+      <ul>
         {/*
           ******* Ex#3 - Étape H ******************************** 
           Utiliser la variable d'état des produits pour générer les composants 'Produit' ci-dessous.
@@ -41,7 +42,15 @@ export default function ListeProduits(props) {
           avoir l'attribut "etatPanier={props.etatPanier}" quand vous les générer ici : encore une fois, regardez 
           le code de l'exercice de classe.
         */}
-
+        {produits.map((prd) => (
+          <Produit
+            id={prd.id}
+            nom={prd.nom}
+            prix={prd.prix}
+            key={prd.id}
+            etatPanier={props.etatPanier}
+          />
+        ))}
       </ul>
     </div>
   );
--- src/data/firebase.js
+++ src/data/firebase.js
@@ -1,18 +1,25 @@
-import fbAppli from 'firebase/app';
-import 'firebase/firestore';
+import fbAppli from "firebase/app";
+import "firebase/firestore";
 
-/******* Ex#3 - Étape B ********************************/ 
+/******* Ex#3 - Étape B ********************************/
 // Ajouter votre objet de configuration Firebase
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyDulzFMxrTidZmWbVkUZIOZ6JNyMZAbB2g",
+  authDomain: "ex3-produits-65dbd.firebaseapp.com",
+  projectId: "ex3-produits-65dbd",
+  storageBucket: "ex3-produits-65dbd.appspot.com",
+  messagingSenderId: "1076197723207",
+  appId: "1:1076197723207:web:7c4abc56fc6c317d80f562",
+  measurementId: "G-GYGJ9RZWKR",
 };
 
 // Initialiser Firebase
-if(!fbAppli.apps.length) {
+if (!fbAppli.apps.length) {
   fbAppli.initializeApp(firebaseConfig);
 }
 
 // Initialiser Firestore
 const bd = fbAppli.firestore();
-/******* Ex#3 - Étape C ********************************/ 
+/******* Ex#3 - Étape C ********************************/
 // Exporter (par défaut) la référence à "bd" pour pouvoir l'utiliser dans le composant ListeProduits
+export default bd;
