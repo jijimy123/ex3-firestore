c41107cc31740e4e9672acc9473035e0f85b325f
jijimy <jijimy123@gmail.com>
2021-03-16 12:04:32 -0400

    enlever config

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -3,15 +3,7 @@ import "firebase/firestore";
 
 /******* Ex#3 - Étape B ********************************/
 // Ajouter votre objet de configuration Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyDulzFMxrTidZmWbVkUZIOZ6JNyMZAbB2g",
-  authDomain: "ex3-produits-65dbd.firebaseapp.com",
-  projectId: "ex3-produits-65dbd",
-  storageBucket: "ex3-produits-65dbd.appspot.com",
-  messagingSenderId: "1076197723207",
-  appId: "1:1076197723207:web:7c4abc56fc6c317d80f562",
-  measurementId: "G-GYGJ9RZWKR",
-};
+const firebaseConfig = {};
 
 // Initialiser Firebase
 if (!fbAppli.apps.length) {
